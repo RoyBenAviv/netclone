526eb08c9343bc159fb6ff84a9359a13c60ec82c
Roy Ben Aviv <roybenavivv@gmail.com>
2022-05-30 19:21:24 +0300

    added all media controls features

--- src/pages/WatchMedia.jsx
+++ src/pages/WatchMedia.jsx
@@ -11,8 +11,14 @@ export const WatchMedia = ({ match }) => {
   const fullScreenBtn = useRef()
   const muteBtn = useRef()
   const volumeRange = useRef()
+  const timeLineContainer = useRef()
+  const timeLeft = useRef()
+  const [duration, setDuration] = useState(null)
   const [volume, setVolume] = useState(1)
+  const [active, setActive] = useState("3")
+  const [wasPaused, setWasPaused] = useState(null)
   const [currentVolume, setCurrentVolume] = useState('high')
+  const [isScrubbing, setIsScrubbing] = useState(false)
 
   const handleChange = (e) => {
     setVolume(e.target.value)
@@ -23,6 +29,12 @@ export const WatchMedia = ({ match }) => {
 
   useEffect(() => {
     loadMedia()
+    document.addEventListener("mouseup", event => {
+      if(isScrubbing) toggleScrubbing(event)
+    })
+    document.addEventListener("mousemove", event => {
+      if(isScrubbing) handleTimeLine(event)
+    })
   }, [])
 
   const loadMedia = async () => {
@@ -63,8 +75,60 @@ export const WatchMedia = ({ match }) => {
     getCurrentVolume()
   }
 
-  const changeSpeed = (speed) => {
+  const changeSpeed = (event, speed) => {
     video.current.playbackRate = speed
+    setActive(event.target.id)
+  }
+
+  const getCurrTime = () => {
+    timeLeft.current.textContent = formatDuration(duration - video.current.currentTime)
+    const percent = video.current.currentTime / duration
+    timeLineContainer.current.style.setProperty('--progress-position', percent)
+  }
+  const getDuration = () => {
+    setDuration(video.current.duration)
+  }
+  const formatDuration = (time) => {
+    const leadingZeroFormatter = new Intl.NumberFormat(undefined,
+     {minimumIntegerDigits: 2,
+     })
+
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time % 60 / 60) % 60
+    const hours = Math.floor(time / 3600)
+
+    return !hours ? `${minutes}:${leadingZeroFormatter.format(seconds)}` : `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`
+
+  }
+
+  const handleTimeLine = (event) => {
+    const rect = timeLineContainer.current.getBoundingClientRect()
+    const percent = Math.min(Math.max(0, event.clientX - rect.x), rect.width) / rect.width
+    timeLineContainer.current.style.setProperty('--preview-position', percent)
+   
+
+    if(isScrubbing) {
+      event.preventDefault()
+      timeLineContainer.current.style.setProperty('--progress-position', percent)
+    }
+  } 
+
+  const skipTime = (duration) => {
+    video.current.currentTime += duration
+  }
+
+  const toggleScrubbing = (event) => {
+    const rect = timeLineContainer.current.getBoundingClientRect()
+    const percent = Math.min(Math.max(0, event.clientX - rect.x), rect.width) / rect.width
+    setIsScrubbing(event.buttons & 1  === 1)
+    if(isScrubbing) {
+      setWasPaused(video.current.paused)
+      video.current.pause()
+    } else {
+      video.current.currentTime = percent * duration
+        if(!wasPaused) video.current.play()
+    }
+    handleTimeLine(event)
   }
 
   if (!media) return <LoadingProfile />
@@ -72,7 +136,14 @@ export const WatchMedia = ({ match }) => {
     <section className="watch-media">
       <div ref={videoContainer} className={'video-container ' + currentVolume}>
         <div className="video-controls">
-          <div className="timeline"></div>
+          <div className='timeline-wrapper'> 
+            <div onMouseDown={event => toggleScrubbing(event)} onMouseMove={(event) => handleTimeLine(event)} ref={timeLineContainer} className="timeline-container">
+              <div className="timeline">
+                <div className="timeline-indicator"></div>
+              </div>
+            <span ref={timeLeft}>0:00</span>
+            </div>
+          </div>
           <div className="controls">
             <div className="left-side">
               <button onClick={() => togglePlay()} ref={playPauseBtn} className="play-pause">
@@ -87,6 +158,12 @@ export const WatchMedia = ({ match }) => {
                 <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="play">
                   <path d="M4 2.69127C4 1.93067 4.81547 1.144851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path>
                 </svg>
+              </button>
+              <button onClick={() => skipTime(-10)}>
+                <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="Hawkins-Icon Hawkins-Icon-Standard"><path fillRule="evenodd" clipRule="evenodd" d="M11.0198 2.04817C13.3222 1.8214 15.6321 2.39998 17.5557 3.68532C19.4794 4.97067 20.8978 6.88324 21.5694 9.09718C22.241 11.3111 22.1242 13.6894 21.2388 15.8269C20.3534 17.9643 18.7543 19.7286 16.714 20.8192C14.6736 21.9098 12.3182 22.2592 10.0491 21.8079C7.77999 21.3565 5.73759 20.1323 4.26989 18.3439C2.80219 16.5555 2 14.3136 2 12L0 12C-2.74181e-06 14.7763 0.962627 17.4666 2.72387 19.6127C4.48511 21.7588 6.93599 23.2278 9.65891 23.7694C12.3818 24.3111 15.2083 23.8918 17.6568 22.5831C20.1052 21.2744 22.0241 19.1572 23.0866 16.5922C24.149 14.0273 24.2892 11.1733 23.4833 8.51661C22.6774 5.85989 20.9752 3.56479 18.6668 2.02238C16.3585 0.479975 13.5867 -0.214319 10.8238 0.057802C8.71195 0.2658 6.70517 1.02859 5 2.2532V1H3V5C3 5.55229 3.44772 6 4 6H8V4H5.99999C7.45608 2.90793 9.19066 2.22833 11.0198 2.04817ZM2 4V7H5V9H1C0.447715 9 0 8.55229 0 8V4H2ZM14.125 16C13.5466 16 13.0389 15.8586 12.6018 15.5758C12.1713 15.2865 11.8385 14.8815 11.6031 14.3609C11.3677 13.8338 11.25 13.2135 11.25 12.5C11.25 11.7929 11.3677 11.1759 11.6031 10.6488C11.8385 10.1217 12.1713 9.71671 12.6018 9.43389C13.0389 9.14463 13.5466 9 14.125 9C14.7034 9 15.2077 9.14463 15.6382 9.43389C16.0753 9.71671 16.4116 10.1217 16.6469 10.6488C16.8823 11.1759 17 11.7929 17 12.5C17 13.2135 16.8823 13.8338 16.6469 14.3609C16.4116 14.8815 16.0753 15.2865 15.6382 15.5758C15.2077 15.8586 14.7034 16 14.125 16ZM14.125 14.6501C14.5151 14.6501 14.8211 14.4637 15.043 14.0909C15.2649 13.7117 15.3759 13.1814 15.3759 12.5C15.3759 11.8186 15.2649 11.2916 15.043 10.9187C14.8211 10.5395 14.5151 10.3499 14.125 10.3499C13.7349 10.3499 13.4289 10.5395 13.207 10.9187C12.9851 11.2916 12.8741 11.8186 12.8741 12.5C12.8741 13.1814 12.9851 13.7117 13.207 14.0909C13.4289 14.4637 13.7349 14.6501 14.125 14.6501ZM8.60395 15.8554V10.7163L7 11.1405V9.81956L10.1978 9.01929V15.8554H8.60395Z" fill="currentColor"></path></svg>
+              </button>
+              <button onClick={() => skipTime(10)}>
+                <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="Hawkins-Icon Hawkins-Icon-Standard"><path fillRule="evenodd" clipRule="evenodd" d="M6.4443 3.68532C8.36794 2.39998 10.6778 1.8214 12.9802 2.04817C14.8093 2.22833 16.5439 2.90793 18 4H16V6H20C20.5523 6 21 5.55228 21 5V1H19V2.2532C17.2948 1.02858 15.288 0.265799 13.1762 0.0578004C10.4133 -0.214321 7.64153 0.479973 5.33315 2.02238C3.02478 3.56479 1.32262 5.85989 0.516716 8.51661C-0.28919 11.1733 -0.148983 14.0273 0.913448 16.5922C1.97588 19.1572 3.8948 21.2744 6.34325 22.5831C8.79169 23.8918 11.6182 24.3111 14.3411 23.7694C17.064 23.2278 19.5149 21.7588 21.2761 19.6127C23.0374 17.4666 24 14.7763 24 12L22 12C22 14.3136 21.1978 16.5555 19.7301 18.3439C18.2624 20.1323 16.22 21.3565 13.9509 21.8079C11.6818 22.2592 9.32641 21.9098 7.28604 20.8192C5.24567 19.7286 3.64657 17.9643 2.76121 15.8269C1.87585 13.6894 1.75901 11.3111 2.4306 9.09717C3.10218 6.88324 4.52065 4.97066 6.4443 3.68532ZM22 4V7H19V9H23C23.5523 9 24 8.55228 24 8V4H22ZM12.6018 15.5758C13.0389 15.8586 13.5466 16 14.125 16C14.7034 16 15.2077 15.8586 15.6382 15.5758C16.0753 15.2865 16.4116 14.8815 16.6469 14.3609C16.8823 13.8338 17 13.2135 17 12.5C17 11.7929 16.8823 11.1758 16.6469 10.6488C16.4116 10.1217 16.0753 9.71671 15.6382 9.43388C15.2077 9.14463 14.7034 9 14.125 9C13.5466 9 13.0389 9.14463 12.6018 9.43388C12.1713 9.71671 11.8385 10.1217 11.6031 10.6488C11.3677 11.1758 11.25 11.7929 11.25 12.5C11.25 13.2135 11.3677 13.8338 11.6031 14.3609C11.8385 14.8815 12.1713 15.2865 12.6018 15.5758ZM15.043 14.0909C14.8211 14.4637 14.5151 14.6501 14.125 14.6501C13.7349 14.6501 13.4289 14.4637 13.207 14.0909C12.9851 13.7117 12.8741 13.1814 12.8741 12.5C12.8741 11.8186 12.9851 11.2916 13.207 10.9187C13.4289 10.5395 13.7349 10.3499 14.125 10.3499C14.5151 10.3499 14.8211 10.5395 15.043 10.9187C15.2649 11.2916 15.3759 11.8186 15.3759 12.5C15.3759 13.1814 15.2649 13.7117 15.043 14.0909ZM8.60395 10.7163V15.8554H10.1978V9.01928L7 9.81956V11.1405L8.60395 10.7163Z" fill="currentColor"></path></svg>
               </button>
               <div className="volume-container">
                 <button onClick={() => toggleMute()} ref={muteBtn} className="mute-btn">
@@ -128,16 +205,19 @@ export const WatchMedia = ({ match }) => {
                 </div>
               </div>
             </div>
+            <div className="middle">
+              <h4>{media.name}</h4>
+            </div>
             <div className="right-side">
               <div className="playback-container">
                 <div className="playback-options">
                   <h3>Playback Speed</h3>
                   <div className="line">
-                    <span onClick={() => changeSpeed(0.5)}></span>
-                    <span onClick={() => changeSpeed(0.75)}></span>
-                    <span className='active' onClick={() => changeSpeed(1)}></span>
-                    <span onClick={() => changeSpeed(1.25)}></span>
-                    <span onClick={() => changeSpeed(1.5)}></span>
+                    <span id={'1'} className={active === '1' ? 'active' : null} onClick={(event) => changeSpeed(event, 0.5)}></span>
+                    <span id={'2'} className={active === '2' ? 'active' : null} onClick={(event) => changeSpeed(event, 0.75)}></span>
+                    <span id={'3'} className={active === '3' ? 'active' : null} onClick={(event) => changeSpeed(event, 1)}></span>
+                    <span id={'4'} className={active === '4' ? 'active' : null} onClick={(event) => changeSpeed(event, 1.25)}></span>
+                    <span id={'5'} className={active === '5' ? 'active' : null} onClick={(event) => changeSpeed(event, 1.5)}></span>
                   </div>
                   <div className="options">
                     <span>0.5x</span>
@@ -174,7 +254,7 @@ export const WatchMedia = ({ match }) => {
             </div>
           </div>
         </div>
-        <video onClick={() => togglePlay()} autoPlay ref={video} playsInline loop src={media.url}></video>
+        <video onClick={() => togglePlay()} onLoadedData={() => getDuration()} onTimeUpdate={() => getCurrTime()} ref={video} autoPlay src={media.url}></video>
       </div>
     </section>
   )
