331659bf4fa841fe7ea989ccab92f3db6e25a69f
Roy Ben Aviv <roybenavivv@gmail.com>
2022-06-02 13:21:20 +0300

    added hash route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <App />
+        <HashRouter>
+                <App />
+        </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
